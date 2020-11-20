89911fd449dde83229c8375731885a2023650853
zlian123 <70665738+zlian123@users.noreply.github.com>
2020-11-20 18:12:57 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# homework7
+# homework_7
+Starter Code from Homework 7 - OpenWeather API.  Instructions can be found at: https://docs.google.com/document/d/1nocC2o_dvAJUyaUk9h4Jh6pMC4j4SjeW3mNp5WKsWbw/edit?usp=sharing
---
+++ js/weather.js
@@ -0,0 +1,55 @@
+function gettingJSON(){
+    //Display the forecast
+    document.querySelector('#forecast').style.display = 'block';
+
+    // Your code here.
+    let apikey = '0fde1a8e3212b9f432db3648867ce795'
+
+    //Set default location if one isn't provided
+    let location = 'Ann Arbor,US';
+
+    // Your code here.
+    if(document.querySelector('#location').value !== '') {
+        location = document.querySelector('#location').value
+    };
+    console.log("Location is : " + location);
+
+    //set default temperature format if one isn't provided
+    // For temperature in Fahrenheit use units=imperial
+    // For temperature in Celsius use units=metric 
+    let format;
+
+    // Your code here.
+    if(document.querySelector('#fahrenheit:checked')) {
+        format = 'imperial'
+    }
+    else if(document.querySelector('#celcius:checked')) {
+        format = 'metric'
+    }
+    else {
+        format = 'imperial';
+    };
+    console.log("Format is " + format);
+
+    //set the query
+    // Your code here.    
+    let query = 'https://api.openweathermap.org/data/2.5/weather?q='+ location +'&units='+ format +'&appid='+ apikey;
+    console.log("Query is :" + query);
+
+    //Create and set variables for each of the elements you
+    //need to update, location, temp, the image, etc.
+    // Your code here.
+
+    $.getJSON(query,function(json){
+        //Use returned json to update the values of the three 
+        //elements in HTML.  
+        //I would print the JSON to the console
+        // Your code here.
+        console.log(JSON.stringify(json))
+        document.querySelector('#loc').innerHTML = json['name'];
+        document.querySelector('#temp').innerHTML = json['main']['temp'];
+        document.querySelector('#tempImg').src = 'http://openweathermap.org/img/wn/' + json['weather'][0]['icon'] +'.png';
+        document.querySelector('#tempImg').alt = json['weather'][0]['description'];
+        // document.querySelector('#tempImg').title = json
+    });
+}
